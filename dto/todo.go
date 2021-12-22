5de02422cfd92e514d115319a4aedd8a6aa04972
hwc9169 <hwc9169@gmail.com>
2021-12-23 00:06:39 +0900

    todo dto 정의

---
+++ dto/todo.go
@@ -0,0 +1,30 @@
+package dto
+
+import (
+	"halill/ent"
+	"time"
+)
+
+type CreateTodoRequest struct {
+	Title    string    `json:"title"`
+	Content  string    `json:"content"`
+	Deadline time.Time `json:"deadline"`
+}
+
+type TodoResponse struct {
+	ID          int64     `json:"id"`
+	Title       string    `json:"title"`
+	Content     string    `json:"content"`
+	Deadline    time.Time `json:"deadline"`
+	IsCompleted bool      `json:"is_completed"`
+}
+
+func TodoToDTO(src *ent.Todo) *TodoResponse {
+	return &TodoResponse{
+		ID:          src.ID,
+		Title:       src.Title,
+		Content:     src.Content,
+		Deadline:    src.Deadline,
+		IsCompleted: src.IsCompleted,
+	}
+}
