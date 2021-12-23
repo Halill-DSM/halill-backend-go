aac5ff70427c647b94e2d6de6fe35ebdf660818a
hwc9169 <hwc9169@gmail.com>
2021-12-24 00:20:13 +0900

    todo repostiroy

--- repository/todo.go
+++ repository/todo.go
@@ -1,18 +1,51 @@
 package repository
 
-import "halill/ent"
+import (
+	"context"
+	"halill/ent"
+	"halill/ent/todo"
+	"halill/ent/user"
+)
 
 type TodoRepository interface {
+	GetAllByEmail(string) ([]*ent.Todo, error)
+	Get(int64) (*ent.Todo, error)
+	Create(*ent.Todo) (*ent.Todo, error)
+	Complete(int64) (*ent.Todo, error)
+	Delete(int64) (*ent.Todo, error)
 }
 
 type todoRepositoryImpl struct {
-	Client *ent.TodoClient
+	db *ent.Client
 }
 
-func NewTodoRepository() TodoRepository {
-	return &todoRepositoryImpl{}
+func NewTodoRepository(db *ent.Client) TodoRepository {
+	return &todoRepositoryImpl{
+		db: db,
+	}
 }
 
-func (r *TodoRepository) FindAllByUserId(userId int) {
-	
+func (r *todoRepositoryImpl) GetAllByEmail(email string) ([]*ent.Todo, error) {
+	result := r.db.Todo.Query().
+		Where(todo.HasUserWith(user.ID(email))).
+		WithUser().
+		AllX(context.TODO())
+
+	return result, nil
+}
+
+func (r *todoRepositoryImpl) Get(todoID int64) (*ent.Todo, error) {
+	return nil, nil
+}
+
+func (r *todoRepositoryImpl) Create(*ent.Todo) (*ent.Todo, error) {
+	return nil, nil
+}
+
+func (r *todoRepositoryImpl) Complete(todoID int64) (*ent.Todo, error) {
+	return nil, nil
+}
+
+func (r *todoRepositoryImpl) Delete(todoID int64) (*ent.Todo, error) {
+	return nil, nil
 }
