1628d70859ac7b7010267e1992846180acbe69d1
hwc9169 <hwc9169@gmail.com>
2021-12-23 22:28:48 +0900

    user 컨트롤러

--- handler/user.go
+++ handler/user.go
@@ -1,5 +1,72 @@
 package handler
 
+import (
+	"halill/dto"
+	"halill/repository"
+	"halill/security"
+	"halill/service"
+
+	"github.com/google/wire"
+	"github.com/labstack/echo"
+)
+
+var UserSet = wire.NewSet(NewUserHandler, service.NewUserSerice, repository.NewUserRepository, security.NewJWTProvider)
+
 type UserHandler struct {
-	
+	us service.UserService
+}
+
+func NewUserHandler(e *echo.Group, us service.UserService) UserHandler {
+	handler := UserHandler{
+		us: us,
+	}
+	e.POST("/login", handler.Login)
+	e.POST("/signup", handler.Register)
+	e.PUT("/login", handler.Refresh)
+	return handler
+}
+
+func (h *UserHandler) Login(c echo.Context) error {
+	request := &dto.LoginRequest{}
+	err := c.Bind(request)
+	if err != nil {
+		return err
+	}
+
+	token, err := h.us.LoginUser(request)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(200, token)
+}
+
+func (h *UserHandler) Register(c echo.Context) error {
+	request := &dto.RegistRequest{}
+	err := c.Bind(request)
+	if err != nil {
+		return err
+	}
+
+	todo, err := h.us.RegistUser(request)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(200, todo)
+}
+
+func (h *UserHandler) Refresh(c echo.Context) error {
+	request := &dto.RefreshTokenRequest{}
+	err := c.Bind(request)
+	if err != nil {
+		return err
+	}
+
+	response, err := h.us.RefreshToken(request)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(200, response)
 }
